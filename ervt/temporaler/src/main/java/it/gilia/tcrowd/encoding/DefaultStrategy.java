8d702c936be53ec9d178890289972a5bca068c2a
German Braun <gab@gab.gab>
2019-10-30 16:12:18 -0300

    adding tests and waiting for running them

--- ervt/temporaler/src/main/java/it/gilia/tcrowd/encoding/DefaultStrategy.java
+++ ervt/temporaler/src/main/java/it/gilia/tcrowd/encoding/DefaultStrategy.java
@@ -8,47 +8,46 @@ package it.gilia.tcrowd.encoding;
  * 
  */
 
-import org.json.simple.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class DefaultStrategy{
+	
+	
+	public DefaultStrategy() {
+		
+	}
 	
 	/**
 	 * 
 	 * @param ervt_json A JSON object representing an ERvt temporal model.
 	 */
-	public to_dllitefpx(String...ervt_json) {
+	public void to_dllitefpx(String ervt_json) {
 		//https://www.mkyong.com/java/json-simple-example-read-and-write-json/
-		JSONParser parser = new JSONParser();
-		JSONObject jsonObject = (JSONObject) parser.parse(ervt_json);
-        System.out.println("Parsing JSON..."+jsonObject);
+		//JSONParser parser = new JSONParser();
+		//JSONObject jsonObject = (JSONObject)parser.parse(ervt_json);
+      //  System.out.println("Parsing JSON..."+jsonObject);
+        System.out.println("Parsing JSON..."+ervt_json);
 
-        String entities = (String) jsonObject.get("entities");
-        System.out.println(name);
+  /*      String entities = (String) jsonObject.get("entities");
+        System.out.println(entities);
 
         String attributes = (String) jsonObject.get("attributes");
         System.out.println(attributes);
 
         String links = (String) jsonObject.get("links");
-        System.out.println(links);
+        System.out.println(links); */
 
         // loop array
-        JSONArray msg = (JSONArray) jsonObject.get("messages");
-        Iterator<String> iterator = msg.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
+        // JSONArray msg = (JSONArray) jsonObject.get("messages");
+        // Iterator<String> iterator = msg.iterator();
+        // while (iterator.hasNext()) {
+           // System.out.println(iterator.next());
+        //}
 		
 	}
 	
-	public entities2dl() {
-		
-	}
-	
-	public attributes2dl() {
-		
-	}
-	
-	public 2dl() {
-		
-	}
 }
