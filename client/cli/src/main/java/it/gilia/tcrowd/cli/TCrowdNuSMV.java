86421a47aa1a975ee0fb07239b426c09ad561a39
German Braun <gab@gab.gab>
2019-11-22 09:31:36 -0300

    code for supporting NuSMV in t-crowd client

--- client/cli/src/main/java/it/gilia/tcrowd/cli/TCrowdNuSMV.java
+++ client/cli/src/main/java/it/gilia/tcrowd/cli/TCrowdNuSMV.java
@@ -13,7 +13,19 @@ import it.unibz.inf.tdllitefpx.TDLLiteFPXConverter;
 import it.unibz.inf.tdllitefpx.output.LatexOutputDocument;
 import it.unibz.inf.tdllitefpx.tbox.TBox;
 
+import it.gilia.tcrowd.encoding.DefaultStrategy;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+import org.json.JSONTokener;
+import org.json.JSONException;
+
 import java.util.Objects;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import org.apache.commons.io.IOUtils;
+
 
 @Command(name = "NuSMV",
         description = "Encode ERvt model into LTL formulae and return a LTL file together with a NuSMV file")
@@ -25,27 +37,45 @@ public class TCrowdNuSMV extends TCrowdEncodingERvtRelatedCommand {
         try {
             Objects.requireNonNull(tModel, "JSON temporal model file must not be null");
 
-    		String fileName="tdl2NuSMV";
+    		String fileNameOut="tcrowdOut";
     		
-    		TBox tBox = new ExampleTDL().getTBox();
+            InputStream is = new FileInputStream(tModel);
+            
+            if (is == null) {
+                throw new NullPointerException("Cannot find resource file " + tModel);
+            }
+            
+            String jsonTxt = IOUtils.toString(is, "UTF-8");
+            System.out.println(jsonTxt);
+
+            JSONObject object = new JSONObject(jsonTxt);
     		
-    		TDLLiteFPXConverter converter = new TDLLiteFPXConverter(tBox);
+    		DefaultStrategy strategy = new DefaultStrategy();
+            TBox tbox = strategy.to_dllitefpx(object);
+    		
+    		TDLLiteFPXConverter converter = new TDLLiteFPXConverter(tbox);
     		Formula qtl = converter.getFormula(true);
+    		
+    		System.out.println("QTL translation...");
+    		(new LatexDocumentCNF(qtl)).toFile(fileNameOut+"_qtl.tex");
     		
     		Formula qtlN =  qtl.makeTemporalStrict();
     		qtlN =  (new NaturalTranslator(qtl)).getTranslation();
+    		
+    		System.out.println("QTL over N translation...");
+    		(new LatexDocumentCNF(qtlN)).toFile(fileNameOut+"_qtlN.tex");
 
     		Formula ltl = qtlN.makePropositional();
-
+    		
     		System.out.println("LTL over N translation...");
-    		(new LatexDocumentCNF(ltl)).toFile(fileName+"_ltl.tex");
+    		(new LatexDocumentCNF(ltl)).toFile(fileNameOut+"_ltl.tex");
     		
     		System.out.println("LTL Formula:");
     		System.out.println("Num of Propositions: "+ltl.getPropositions().size());
     		System.out.println("Num of Constants: "+ltl.getConstants().size());
     		
     		System.out.println("NuSMV file...");
-    		(new NuSMVOutput(ltl)).toFile(fileName+".smv");
+    		(new NuSMVOutput(ltl)).toFile(fileNameOut+".smv");
     		
 
         } catch (Exception e) {
