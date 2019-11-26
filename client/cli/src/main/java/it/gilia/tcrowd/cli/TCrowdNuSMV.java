f3d6907efabab42a0a873d4374d53d1f70c7792e
German Braun <gab@gab.gab>
2019-11-26 12:14:25 -0300

    redefining NuSMV command: query file is given as input. Input file means satisfiability checking and non empty file must include a single concept (up to now) to be checked

--- client/cli/src/main/java/it/gilia/tcrowd/cli/TCrowdNuSMV.java
+++ client/cli/src/main/java/it/gilia/tcrowd/cli/TCrowdNuSMV.java
@@ -10,7 +10,9 @@ import it.unibz.inf.qtl1.output.LatexDocumentCNF;
 import it.unibz.inf.qtl1.output.NuSMVOutput;
 import it.unibz.inf.tdllitefpx.ExampleTDL;
 import it.unibz.inf.tdllitefpx.TDLLiteFPXConverter;
+import it.unibz.inf.tdllitefpx.TDLLiteFPXReasoner;
 import it.unibz.inf.tdllitefpx.output.LatexOutputDocument;
+import it.unibz.inf.tdllitefpx.concepts.Concept;
 import it.unibz.inf.tdllitefpx.tbox.TBox;
 
 import it.gilia.tcrowd.encoding.DefaultStrategy;
@@ -22,14 +24,23 @@ import org.json.JSONStringer;
 import org.json.JSONTokener;
 import org.json.JSONException;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.FileInputStream;
+import java.io.BufferedReader;
 import org.apache.commons.io.IOUtils;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 @Command(name = "NuSMV",
-        description = "Encode ERvt model into LTL formulae and return a LTL file together with a NuSMV file")
+        description = "Encode ERvt model into LTL formulae and return a LTL file together with a NuSMV file including"
+        		+ "the query given as an input file. If file is empty, TBox is to be checked for satisifiability."
+        		+ "Otherwise, query must be a concept to be checked.")
 public class TCrowdNuSMV extends TCrowdEncodingERvtRelatedCommand {
 
     @Override
@@ -37,6 +48,7 @@ public class TCrowdNuSMV extends TCrowdEncodingERvtRelatedCommand {
 
         try {
             Objects.requireNonNull(tModel, "JSON temporal model file must not be null");
+            Objects.requireNonNull(queryF, "Query file must not be null");
     		
             InputStream is = new FileInputStream(tModel);
             
@@ -55,30 +67,35 @@ public class TCrowdNuSMV extends TCrowdEncodingERvtRelatedCommand {
     		
     		DefaultStrategy strategy = new DefaultStrategy();
             TBox tbox = strategy.to_dllitefpx(object);
-    		
-    		TDLLiteFPXConverter converter = new TDLLiteFPXConverter(tbox);
-    		Formula qtl = converter.getFormula(true);
-    		
-    		System.out.println("QTL translation...");
-    		(new LatexDocumentCNF(qtl)).toFile(fileNameOut+"_qtl.tex");
-    		
-    		Formula qtlN =  qtl.makeTemporalStrict();
-    		qtlN =  (new NaturalTranslator(qtl)).getTranslation();
-    		
-    		System.out.println("QTL over N translation...");
-    		(new LatexDocumentCNF(qtlN)).toFile(fileNameOut+"_qtlN.tex");
+            
+            InputStream query = new FileInputStream(queryF);
+            
+            if (query == null) {
+                throw new NullPointerException("Cannot find query file " + query);
+            }
+            else {
+            	BufferedReader reader = new BufferedReader(new FileReader(queryF));
+            	String line = reader.readLine();
+           	    
+            	if (line == null) { /*Check for TBox satisfiability */
+            	    System.out.println("No errors, and file empty");
+            	    
+            	    TDLLiteFPXReasoner.buildCheckSatisfiability(
+            	    		tbox,
+            	    		true, 
+            	    		fileNameOut);
+            	    
+            	}else { /*Check for Concept satisfiability */
+            		System.out.println(line);
 
-    		Formula ltl = qtlN.makePropositional();
-    		
-    		System.out.println("LTL over N translation...");
-    		(new LatexDocumentCNF(ltl)).toFile(fileNameOut+"_ltl.tex");
-    		
-    		System.out.println("LTL Formula:");
-    		System.out.println("Num of Propositions: "+ltl.getPropositions().size());
-    		System.out.println("Num of Constants: "+ltl.getConstants().size());
-    		
-    		System.out.println("NuSMV file...");
-    		(new NuSMVOutput(ltl)).toFile(fileNameOut+".smv");
+            		Concept acpt = strategy.giveMeAconcept(line); 
+            		TDLLiteFPXReasoner.buildCheckConceptSatisfiability(
+            				tbox,
+            				acpt,
+            				true, 
+            				fileNameOut);
+            	}
+            }
     		
 
         } catch (Exception e) {
