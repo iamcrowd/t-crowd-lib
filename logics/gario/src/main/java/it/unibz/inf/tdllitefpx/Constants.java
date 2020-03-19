d5d4c9fe3a6dce566662669b60a6090113c5edf4
German Braun <gab@gab.gab>
2020-03-19 11:23:45 -0300

    constants for solver names

---
+++ logics/gario/src/main/java/it/unibz/inf/tdllitefpx/Constants.java
@@ -0,0 +1,17 @@
+package it.unibz.inf.tdllitefpx;
+
+/**
+ * It encapsulates constants for SAT solver names
+ * The only purpose of this class is to organize the constants (specially string constants)
+ *
+ * @author gab
+ */
+public class Constants {
+
+    // SAT Solvers
+    public static final String NuSMV = "NuSMV";
+    public static final String Aalta = "Aalta";
+    public static final String pltl = "pltl";
+    public static final String TRP = "TRP";
+
+}
