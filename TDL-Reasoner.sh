b58d2088da30dde0e99a20e5c957df2342a65cd4
German Braun <gab@gab.gab>
2020-03-20 12:48:53 -0300

    I added help, status and now files are removed before running exmaples

--- TDL-Reasoner.sh
+++ TDL-Reasoner.sh
@@ -25,6 +25,24 @@ if [ $# -eq 0 ]
     exit
 fi
 
+if [ $1 == "status" ];
+then
+    echo -e "\t \t \\e[0;43m**Status**\\e[0m"
+    echo ""
+    echo -e "KB = <TBox, {}> and NuSMV solver. PLTL (QTL1 -> PLTL) is working BUT fails if TBox does not include roles"
+    echo ""
+    echo -e "KB = <TBox, {}> and NuSMV solver. LTL (QTL1 -> QTLN -> LTL) is working BUT fails if TBox does not include roles"
+    echo ""
+    echo -e "KB = <TBox, ABox> and NuSMV solver. PLTL (QTL1 -> PLTL) is working"
+    echo ""
+    echo -e "KB = <TBox, ABox> and NuSMV solver. LTL (QTL1 -> QTLN -> LTL) to be implemented (BUG)"
+    echo ""
+    echo -e "KB = <TBox, {}> and Aalta solver. LTL (QTL1 -> QTLN -> LTL) is working BUT fails if TBox does not include roles"
+    echo ""
+    echo -e "KB = <TBox, ABox> and Aalta solver. LTL (QTL1 -> QTLN -> LTL) to be implemented (BUG)"
+    exit
+fi
+
 if [ $1 == "help" ];
 then
     echo -e "\t \t \\e[0;43m**Help me to run**\\e[0m"
@@ -60,17 +78,26 @@ then
     echo -e "\t \t - 'false': \t TDL-Lite -> QLT1 -> PLTL"
     echo ""
     echo -e "\\e[0;43mTODO:\\e[0m"
+    echo ""
+    echo -e "\t - BUG 1: Checking SAT TBox and ABox LTL pure future is not working (both solvers)"
+    echo -e "\t - BUG 2: Checking SAT TBox and Empty ABox if TBox does not include roles outs pointer null"
+    echo ""
     echo -e "\t - Checking SAT TBox and ABox LTL pure future. Currently only works for PTLT and NuSMV"
-    echo -e "\t - Checking SAT TBox using NuSMV should not use Gario's translation. Currently it keeps Y operator"
+    echo -e "\t - We have to review the NuXMV options. Currently, bmc "
     echo -e "\t - Add more solvers: pltl (tableaux-based) | TRP++ (temporal resolution)"
     exit
 fi
 
 
+#Remove previous files from example folder
 
+find . -name "*.smv" -type f -delete
+find . -name "*.aalta" -type f -delete
+find . -name "*.tex" -type f -delete
 
 echo "Current example directory: "
 ls -l $1
+
 
 echo "Do you want to continue? (Y|N)"
 read yn
@@ -88,63 +115,44 @@ fi
         echo -e "\\e[0;41mTBox not found. Exit!\\e[0m"
         exit
     fi
+    
+    aboxfile="${1}abox.json"
+
+    if [ ! -f "$aboxfile" ];
+    then
+        echo -e "\\e[0;41mABox not found. Exit!\\e[0m"
+        exit
+    fi
+    
+
+### if solver is NuSMV
 
 if [ $2 -eq 1 ];
 then
     echo -e "\\e[0;41mSolver selected: NuSMV\\e[0m"
 
-    aboxfile="${1}abox.json"
+    echo -e "\\e[0;42mPlease, enter true|false to give a pure future or a pltl formulae to NuSMV\\e[0m"
 
-    if [ -f "$aboxfile" ];
+    read purefuture
+
+    if [ $purefuture = true ];
     then
-    
-        echo -e "\\e[0;42mPlease, enter true|false to give a pure future or a pltl formulae to NuSMV\\e[0m"
-
-        read purefuture
-
-        if [ $purefuture = true ];
-        then
-            echo -e "\\e[0;48mThis option has not been yet developed. Exit!\\e[0m"
-            exit
-                #java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxABoxSatNuSMV -t "${1}tbox.json" -a "${1}abox.json" -pf
-
-         else
-             if [ $purefuture = false ];
-             then
-                java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxABoxSatNuSMV -t "${1}tbox.json" -a "${1}abox.json"
-             else
-                 echo "You must enter true|false"
-                 exit
-             fi
-         fi
+       java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxABoxSatLTL -t "${1}tbox.json" -a "${1}abox.json" -s NuSMV 
 
     else
-        echo -e "\\e[0;42mPlease, enter true|false to give a pure future or a pltl formulae to NuSMV\\e[0m"
-
-        read purefuture
-
-        if [ $purefuture = true ];
-        then
-            echo -e "\\e[0;48mpure future formulae\\e[0m"
-            java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxSat -t "${1}tbox.json" -q "${1}query.txt" -pf -s NuSMV
-        
-        else
-            if [ $purefuture = false ];
-            then
-                echo -e "\\e[0;48mPLTL formulae\\e[0m"
-                java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxSat -t "${1}tbox.json" -q "${1}query.txt" -s NuSMV
-            else 
-                echo "You must enter true|false"
-                exit
-            fi
-        fi
+       if [ $purefuture = false ];
+       then
+          java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxABoxSatPLTL -t "${1}tbox.json" -a "${1}abox.json"
+       else
+    	   echo "You must enter true|false"
+           exit
+       fi
     fi
-
 
     file="${1}tcrowdOut.smv"
     if [ -f "$file" ];
     then
-	    ./solvers/NuSMV/NuSMV -dcx -bmc -bmc_length 11 "${1}tcrowdOut.smv"
+	    ./solvers/NuSMV/NuSMV -dcx -dynamic "${1}tcrowdOut.smv"
     else
 	    echo "$file not found."
         exit
@@ -156,29 +164,20 @@ else
     if [ $2 -eq 2 ];
     then
         echo -e "\\e[0;42mSolver selected: Aalta v2.0\\e[0m"
+           
+        java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxABoxSatLTL -t "${1}tbox.json" -a "${1}abox.json" -s Aalta 
 
-        aboxfile="${1}abox.json"
+		file="${1}tcrowdOut.aalta"
+    
+    	if [ -f "$file" ];
+    	then
+	  		./solvers/Aalta/aalta cat "${1}tcrowdOut.aalta"
 
-        if [ -f "$aboxfile" ];
-        then
-            echo -e "\\e[0;48mThis option has not been yet developed. Exit!\\e[0m"
-            exit
-
-        #java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxSat -t "${1}tbox.json" -q "${1}query.txt" -pf -s Aalta
-        else
-           java -cp t-crowd-cli-4.0.0-SNAPSHOT.jar it.gilia.tcrowd.cli.TCrowd TBoxSat -t "${1}tbox.json" -q "${1}query.txt" -pf -s Aalta 
-        fi
-
-        file="${1}tcrowdOut.aalta"
-        if [ -f "$file" ];
-        then
-	        ./solvers/Aalta/aalta cat "${1}tcrowdOut.aalta"
-
-        else
-	        echo "$file not found."
-            exit
-        fi
-    else
+    	else
+	    	echo "$file not found."
+        	exit
+    	fi
+   	else
        echo "Invalid solver."
        exit
     fi    
