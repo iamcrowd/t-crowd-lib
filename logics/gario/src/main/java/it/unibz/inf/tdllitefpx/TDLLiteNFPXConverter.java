812de663a717f472d1548555436f6acdfb12afb5
gbraun <gbraun@gilia.gilia>
2021-12-30 10:27:08 -0300

    Removing useless code

--- logics/gario/src/main/java/it/unibz/inf/tdllitefpx/TDLLiteNFPXConverter.java
+++ logics/gario/src/main/java/it/unibz/inf/tdllitefpx/TDLLiteNFPXConverter.java
@@ -109,7 +109,6 @@ public class TDLLiteNFPXConverter {
 			F = new ConjunctiveFormula(getT(), 
 									  getEpsilon());
 		}
-		System.out.println("Formula F final in Converter"+F.toString());
 		return F;
 	}
 	
@@ -217,8 +216,6 @@ public class TDLLiteNFPXConverter {
 	*/
 	public Formula getEpsilonX(){
 		Formula F = new ConjunctiveFormula(getFactorizedT(), epsX);
-
-		System.out.println("GetEpsilonX "+F.toString());
 		return F;
 	}
 	
@@ -313,12 +310,7 @@ public class TDLLiteNFPXConverter {
 
 		for( Role s : tbox.getRoles()){
 
-
-			System.out.println("s in getFactorizedEpsilon "+s.toString());
-
 			if(s instanceof PositiveRole){
-
-				System.out.println("s is PositiveRole in getFactorizedEpsilon "+s.toString());
 				
 				Proposition pS = (Proposition) a.get("P"+s.toString(),0);
 				Proposition pinvS = (Proposition) a.get("Pinv"+s.toString(),0);
@@ -377,19 +369,13 @@ public class TDLLiteNFPXConverter {
 	 */
 	public void getExtendedFormula(){
 		Set<QuantifiedRole> qRoles= tbox.getQuantifiedRoles1();
-
-		System.out.println("set of qRoles in addExtensionConstraints:"+qRoles.toString());
 		
 		/* 
 		 * (2)
 		 */
 		Map<Role,List<QuantifiedRole>> qRMap = new HashMap<Role, List<QuantifiedRole>>();
-		
-		System.out.println("qRMap in addExtensionConstraints "+qRMap.toString());
 
 		for(QuantifiedRole qR : qRoles){
-
-			System.out.println("qR in addExtensionConstraints "+qR.toString());
 
 			List<QuantifiedRole> list = qRMap.get(qR.getRole());
 
@@ -398,13 +384,10 @@ public class TDLLiteNFPXConverter {
 				qRMap.put(qR.getRole(),list);
 			}
 			list.add(qR);
-			System.out.println("list of QuantifiedRole in AddExtension"+list.toString());
 		}
 		
 		for(Entry<Role, List<QuantifiedRole>> e: qRMap.entrySet()){
 			List<QuantifiedRole> qrL = e.getValue();
-
-			System.out.println("qrL in AddExtension"+qrL.toString());
 
 			Collections.sort(qrL, new Comparator<QuantifiedRole>() {
 				@Override
@@ -417,8 +400,6 @@ public class TDLLiteNFPXConverter {
 				cardinalities.add(new ImplicationFormula(
 						conceptToFormula(qrL.get(i)),
 						conceptToFormula(qrL.get(i + 1))));
-
-				System.out.println("Extending TBox Checking (2) in formula"+qrL.get(i)+" "+qrL.get(i+1));
 			}
 		}
 		
@@ -431,10 +412,6 @@ public class TDLLiteNFPXConverter {
 
 		for(QuantifiedRole qR : qRoles){
 
-			System.out.println("Set of Roles in (3) extended formula"+roles.toString());
-
-			System.out.println("Exending TBox Checking (3) in formula (roles)"+qR.toString());
-
 			if(qR.getRole().getRefersTo() instanceof AtomicRigidRole){
 
 				roles.remove(qR.getRole());
@@ -443,17 +420,12 @@ public class TDLLiteNFPXConverter {
 							new it.unibz.inf.qtl1.formulae.temporal.SometimeFuture(conceptToFormula(qR)), 
 							new it.unibz.inf.qtl1.formulae.temporal.AlwaysFuture(conceptToFormula(qR))
 							)
-						);
-
-				System.out.println("Exending TBox Checking (3) in formula (if roles are rigid)"+rigidR.toString());    
+						);   
 			}
 		}
-		System.out.println("Set of Roles in (3) extended formula after removing"+roles.toString());
 		
 		for (Role role : roles) {
 			if(role.getRefersTo() instanceof AtomicRigidRole){
-
-				System.out.println("Exending TBox Checking (3) for remaining rigid roles"+role.toString());
 
 				QuantifiedRole qRoleRem = new QuantifiedRole(role, 1);
 
@@ -462,8 +434,6 @@ public class TDLLiteNFPXConverter {
 								new it.unibz.inf.qtl1.formulae.temporal.AlwaysFuture(conceptToFormula(qRoleRem))
 								)
 							);
-			
-				System.out.println("Exending TBox Checking (3) in formula (if roles are rigid)"+rigidR.toString());
 			}				
 		}
 		
