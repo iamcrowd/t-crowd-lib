bad53d6213250f3eddf05cc7a2022454755a040a
gbraun <gbraun@gilia.gilia>
2022-03-04 13:06:48 -0300

    import OWL: Now rewriting equivalences as subclass axioms to import them. Tests added.

--- owl/importer/src/test/java/it/gilia/tcrowd/importer/OWLImportTest.java
+++ owl/importer/src/test/java/it/gilia/tcrowd/importer/OWLImportTest.java
@@ -8,20 +8,59 @@ import org.junit.jupiter.api.*;
 
 import org.semanticweb.owlapi.model.*;
 
+import it.unibz.inf.tdllitefpx.output.LatexOutputDocument;
+
 
 @DisplayName("Test Suite")
 public class OWLImportTest {
 	
+    // http://owl.man.ac.uk/2006/07/sssw/people
+    // https://protege.stanford.edu/ontologies/pizza/pizza.owl
     @Test
-    public void testPrintCI(){
+    public void testImportPizza(){
         try {
             IRI ontoiri = IRI.create("https://protege.stanford.edu/ontologies/pizza/pizza.owl");
 			OWLImport importer = new OWLImport();
             importer.load(ontoiri);
 			importer.dlliteCI();
+            try{
+				(new LatexOutputDocument(importer.getTBox())).toFile("pizza.tex");
+			} catch (Exception e) {}
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testImportPeople(){
+        try {
+            IRI ontoiri = IRI.create("http://owl.man.ac.uk/2006/07/sssw/people.owl");
+			OWLImport importer = new OWLImport();
+            importer.load(ontoiri);
+			importer.dlliteCI();
+            try{
+				(new LatexOutputDocument(importer.getTBox())).toFile("people.tex");
+			} catch (Exception e) {}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testToyFromFile() {
+        try {
+            String path = new String(OWLImport.class.getClassLoader().getResource("ontologies/toyTestDLLite.owl").toString());
+            String[] owlfilepath = path.split(":", 2);
+            OWLImport importer = new OWLImport();
+			importer.loadFromPath(owlfilepath[1]);
+			importer.dlliteCI();
+            try{
+				(new LatexOutputDocument(importer.getTBox())).toFile("toyTestDLLite.tex");
+			} catch (Exception e) {}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
