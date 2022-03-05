f275f34a34c6cd90d97b411a6755fad7be0c914e
gbraun <gbraun@gilia.gilia>
2022-03-04 23:25:59 -0300

    first approach to DLLite REasoner. Putting all together. Toy test without roles working

--- owl/importer/src/test/java/it/gilia/tcrowd/importer/DLLiteTest.java
+++ owl/importer/src/test/java/it/gilia/tcrowd/importer/DLLiteTest.java
@@ -2,6 +2,7 @@ package it.gilia.tcrowd.importer;
 
 
 import org.junit.jupiter.api.Test;
+import org.semanticweb.owlapi.model.IRI;
 import org.junit.jupiter.api.*;
 
 import java.util.Map;
@@ -25,11 +26,76 @@ public class DLLiteTest {
 				(new LatexOutputDocument(importer.getTBox())).toFile("AdultExampleDLLite.tex");
 			} catch (Exception e) {}
 
-			DLLiteReasoner.buildCheckTBoxSat(
+			DLLiteReasoner.checkTBoxSat(
 				importer.getTBox(), 
 				true, 
-				"Adult", 
-				"Black");
+				"Adult",
+				"all");
+
+
+			Map<String, String> stats = importer.getTBox().getStats();
+			System.out.println("");
+			System.out.println("------TBOX------");
+			String key;
+			key = "Basic Concepts:";
+			System.out.println(key + stats.get(key));
+			key = "Roles:";
+			System.out.println(key + stats.get(key));
+			key = "CIs:";
+			System.out.println(key + stats.get(key));
+		} catch (Exception e) {}
+	}
+
+	@Test
+	public void StudentDLLiteTest(){
+		try{
+			String path = new String(OWLImport.class.getClassLoader().getResource("ontologies/toyDomainRange.owl").toString());
+        	String[] owlfilepath = path.split(":", 2);
+        	OWLImport importer = new OWLImport();
+			importer.loadFromPath(owlfilepath[1]);
+			importer.dlliteCI();
+
+			try{
+				(new LatexOutputDocument(importer.getTBox())).toFile("StudentDLLite.tex");
+			} catch (Exception e) {}
+
+			DLLiteReasoner.checkTBoxSat(
+				importer.getTBox(), 
+				true, 
+				"Student",
+				"all");
+
+
+			Map<String, String> stats = importer.getTBox().getStats();
+			System.out.println("");
+			System.out.println("------TBOX------");
+			String key;
+			key = "Basic Concepts:";
+			System.out.println(key + stats.get(key));
+			key = "Roles:";
+			System.out.println(key + stats.get(key));
+			key = "CIs:";
+			System.out.println(key + stats.get(key));
+		} catch (Exception e) {}
+	}
+
+	@Test
+	public void PizzaDLLiteTest(){
+		try{
+			IRI ontoiri = IRI.create("https://protege.stanford.edu/ontologies/pizza/pizza.owl");
+			OWLImport importer = new OWLImport();
+			importer.load(ontoiri);
+			importer.dlliteCI();
+
+			try{
+				(new LatexOutputDocument(importer.getTBox())).toFile("pizzaDLLlite.tex");
+			} catch (Exception e) {}
+
+			DLLiteReasoner.checkTBoxSat(
+				importer.getTBox(), 
+				true, 
+				"Pizza",
+				"all");
 
 
 			Map<String, String> stats = importer.getTBox().getStats();
