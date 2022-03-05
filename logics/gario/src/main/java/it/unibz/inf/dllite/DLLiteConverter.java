1f428781f5a3f3ea3b08af6244dd2d855df77cf9
gbraun <gbraun@gilia.gilia>
2022-03-04 23:24:40 -0300

    first approach to DLLite REasoner. Trying whole pipeline. Changes on the QTL formula, removing temporal operators

---
+++ logics/gario/src/main/java/it/unibz/inf/dllite/DLLiteConverter.java
@@ -0,0 +1,348 @@
+package it.unibz.inf.dllite;
+
+import it.unibz.inf.qtl1.atoms.Atom;
+import it.unibz.inf.qtl1.atoms.Bot;
+import it.unibz.inf.qtl1.atoms.Proposition;
+import it.unibz.inf.qtl1.formulae.Alphabet;
+import it.unibz.inf.qtl1.formulae.ConjunctiveFormula;
+import it.unibz.inf.qtl1.formulae.Formula;
+import it.unibz.inf.qtl1.formulae.ImplicationFormula;
+import it.unibz.inf.qtl1.formulae.NegatedFormula;
+import it.unibz.inf.qtl1.formulae.quantified.UniversalFormula;
+import it.unibz.inf.qtl1.formulae.temporal.Always;
+import it.unibz.inf.qtl1.terms.Constant;
+import it.unibz.inf.qtl1.terms.Variable;
+import it.unibz.inf.tdllitefpx.TDLLiteNFPXConverter;
+import it.unibz.inf.tdllitefpx.concepts.AtomicConcept;
+import it.unibz.inf.tdllitefpx.concepts.BottomConcept;
+import it.unibz.inf.tdllitefpx.concepts.Concept;
+import it.unibz.inf.tdllitefpx.concepts.ConjunctiveConcept;
+import it.unibz.inf.tdllitefpx.concepts.NegatedConcept;
+import it.unibz.inf.tdllitefpx.concepts.QuantifiedRole;
+import it.unibz.inf.tdllitefpx.concepts.temporal.AlwaysFuture;
+import it.unibz.inf.tdllitefpx.concepts.temporal.AlwaysPast;
+import it.unibz.inf.tdllitefpx.concepts.temporal.NextFuture;
+import it.unibz.inf.tdllitefpx.concepts.temporal.NextPast;
+import it.unibz.inf.tdllitefpx.concepts.temporal.SometimeFuture;
+import it.unibz.inf.tdllitefpx.concepts.temporal.SometimePast;
+import it.unibz.inf.tdllitefpx.concepts.temporal.TemporalConcept;
+import it.unibz.inf.tdllitefpx.roles.PositiveRole;
+import it.unibz.inf.tdllitefpx.roles.AtomicRigidRole;
+import it.unibz.inf.tdllitefpx.roles.Role;
+import it.unibz.inf.tdllitefpx.tbox.ConceptInclusionAssertion;
+import it.unibz.inf.tdllitefpx.tbox.TBox;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+
+public class DLLiteConverter extends TDLLiteNFPXConverter{
+	
+//	TBox tbox;
+//	Alphabet a;
+//	Variable x;
+	
+	ConjunctiveFormula cardinalities = new ConjunctiveFormula();
+	ConjunctiveFormula rigidR = new ConjunctiveFormula();
+	ConjunctiveFormula epsX = new ConjunctiveFormula();
+	ConjunctiveFormula eps = new ConjunctiveFormula();
+	
+	public DLLiteConverter(TBox tbox){
+		super(tbox);		
+	}
+
+	/**
+	 * Returns a K+ given an TBox T. 
+	 * If factorize is set to true, the formula returned will
+	 * have only 1 outermost universal quantifier, and the rest
+	 * will be quantifier free. Moreover less box operators will be used
+	 * 
+	 * The result formula has the following structure
+	 * F = T /\ e
+	 * 
+	 * where
+	 * 
+	 * T = /\ \boxF \forall x ( C1(x) -> C2(x) )  			(1)
+	 * 		(For each concept inclusion in T*)
+	 * 
+	 * 	   /\ \boxF \forall x ( Eq'S(x) -> EqS(x) )  		(2)
+	 * 		(for each q,q' \in Qrt with q' > q. 
+	 * 		 there is no q'' in Qrt with q < q'' < q')
+	 * 
+	 * 	   /\ \forall x ( \diamondF EqS(x) -> \boxF EqS(x) )  (3)
+	 * 		(for each global/ridig role s \in Qrt) 
+	 * 
+	 * e = /\ epsilon(s)    (For each role S) 				(4)
+	 *    
+	 *
+	 * @param factorize
+	 * @return
+	 */
+	public Formula getFormula(){
+		Formula F;
+
+		System.out.println("Estoy aca!!");
+
+		this.getFactorizedEpsilon();
+
+		System.out.println("Estoy aca!!");
+
+		this.getExtendedFormula();
+		System.out.println("Estoy aca!!");
+
+		F = new ConjunctiveFormula(this.getFactorizedT(), cardinalities);
+		F = new UniversalFormula(
+								new ConjunctiveFormula(F,
+										new ConjunctiveFormula(epsX, 
+															   rigidR)),
+						 super.x); 
+		F = new ConjunctiveFormula(F, eps);
+		return F;
+	}
+	
+	private Formula getFactorizedT(){
+		/*
+		 * Returns \box \bigwedge c1->c2
+		 * If we put the outer always here, we left out of the scope to epsilon(x) formulas
+		 */
+		
+		ConjunctiveFormula out = new ConjunctiveFormula();
+		for(ConceptInclusionAssertion ci: super.tbox){
+			/* 
+			 * Special cases are Top -> A and B -> bot 
+			 */
+			if(ci.getLHS() instanceof NegatedConcept && 
+					((NegatedConcept)ci.getLHS()).getRefersTo() instanceof BottomConcept){
+				out.add(conceptToFormula(ci.getRHS()));
+			}else if(ci.getRHS() instanceof BottomConcept){
+				out.add(new NegatedFormula(conceptToFormula(ci.getLHS())));
+			}else
+				out.add(new ImplicationFormula(
+								conceptToFormula(ci.getLHS()),
+								conceptToFormula(ci.getRHS())));
+		}
+
+		return out;
+	}
+
+	/**
+	 * Converting TDLLite concetps into QTL1 formulas
+	 * 
+	 * @param c a TDLlite Concept
+	 * @return a QTL1 formula
+	 */
+	public Formula conceptToFormula(Concept c){
+		if(c instanceof AtomicConcept)
+			return new Atom(c.toString(), super.x);
+		else if(c instanceof QuantifiedRole){
+			QuantifiedRole qE = (QuantifiedRole)c;
+			Atom atom = super.a.get("E"+qE.getQ()+qE.getRole().toString(),1);
+			atom.setArg(0, super.x);
+			return atom;
+		}
+		else if(c instanceof BottomConcept)
+			return Bot.getStatic();
+		else if(c instanceof NegatedConcept){
+			NegatedConcept nc = (NegatedConcept) c;
+			return new NegatedFormula(conceptToFormula(nc.getRefersTo()));
+		}
+		else if(c instanceof ConjunctiveConcept){
+			ConjunctiveConcept cc = (ConjunctiveConcept)c;
+			ConjunctiveFormula cf = new ConjunctiveFormula();
+			for(Concept d : cc.getConjuncts()){
+				cf.addConjunct(conceptToFormula(d));
+			}
+			return cf;
+		}
+		else if(c instanceof TemporalConcept){
+			TemporalConcept d = (TemporalConcept) c;
+			if(c instanceof NextFuture){
+				return new it.unibz.inf.qtl1.formulae.temporal.NextFuture(
+						conceptToFormula(d.getRefersTo()));
+			}else if(c instanceof NextPast){
+				return new it.unibz.inf.qtl1.formulae.temporal.NextPast(
+						conceptToFormula(d.getRefersTo()));
+			}else if(c instanceof AlwaysPast){
+				return new it.unibz.inf.qtl1.formulae.temporal.AlwaysPast(
+						conceptToFormula(d.getRefersTo()));
+			}else if(c instanceof AlwaysFuture){
+				return new it.unibz.inf.qtl1.formulae.temporal.AlwaysFuture(
+						conceptToFormula(d.getRefersTo()));
+			}else if(c instanceof SometimePast){
+				return new it.unibz.inf.qtl1.formulae.temporal.SometimePast(
+						conceptToFormula(d.getRefersTo()));
+			}else if(c instanceof SometimeFuture){
+				return new it.unibz.inf.qtl1.formulae.temporal.SometimeFuture(
+						conceptToFormula(d.getRefersTo()));
+			}else if(c instanceof it.unibz.inf.tdllitefpx.concepts.temporal.Always){
+				return new it.unibz.inf.qtl1.formulae.temporal.Always(
+						conceptToFormula(d.getRefersTo()));
+			}
+		}
+		
+		System.err.println("Unexpected case "+c.getClass().toString());
+		return null;
+			
+			
+	}
+
+	/** 
+	 * Gathering formula with the variable "x"
+	*/
+	public Formula getEpsilonX(){
+		Formula F = new ConjunctiveFormula(getFactorizedT(), epsX);
+		return F;
+	}
+	
+	/**
+	 * Gathering the formula with the variable "x" to avoid redundancy 
+	 * formula when make it propositional
+	*/
+	public Formula getEpsilonWithoutX(){
+		return eps;
+	}
+	
+	/* A reduced version (with less modal operators) :
+		*
+		*  with epsilon'(S) = (\box \forall x ((E1S(x)-> \box pS ))) /\ (pinvS E1SInv(x) -> E1S(ds))
+		*
+		* epsilon''(S) =(\box \forall x (E1SInv(x)-> \box pSinv )) /\ (pS -> E1SInv(dsinv))
+	*/
+	private void getFactorizedEpsilon(){
+
+		for(Role s : super.tbox.getRoles()){
+
+			if(s instanceof PositiveRole){
+				
+				Proposition pS = (Proposition) super.a.get("P"+s.toString(),0);
+				Proposition pinvS = (Proposition) super.a.get("Pinv"+s.toString(),0);
+				Role SInv = s.getInverse();
+
+				Concept E1S = new QuantifiedRole(s, 1);
+				Concept E1SInv = new QuantifiedRole(SInv, 1);
+
+				Constant ds = new Constant("d"+s.toString());
+				Constant dsinv = new Constant("d"+SInv.toString());
+
+
+
+				Formula fE1S_ds = conceptToFormula(E1S);
+				fE1S_ds.substitute(super.x, ds);
+
+				Formula fE1SInv_ds = conceptToFormula(E1SInv);
+				fE1SInv_ds.substitute(super.x, dsinv);
+
+	
+
+				epsX.add(new ImplicationFormula(
+											conceptToFormula(E1S),
+					 						pS
+										   )
+					);
+
+				epsX.add(new ImplicationFormula(
+											conceptToFormula(E1SInv),
+					 						pinvS
+										   )
+					);
+
+				eps.add(new ImplicationFormula(
+											pinvS,
+											fE1S_ds));
+			
+				eps.add(new ImplicationFormula(
+											pS,
+											fE1SInv_ds));
+
+				}
+			}
+		}
+	
+	/***
+	 * Reduction to QTL1
+	 * Assert cardinality axioms (2) and rigid roles axioms (3) as explained in the report.
+	 * 
+	 *     /\ \boxPF \forall x ( Eq'S(x) -> EqS(x) )  		(2)
+	 * 		(for each q,q' \in Qrt with q' > q. 
+	 * 		 there is no q'' in Qrt with q < q'' < q')
+	 * 
+	 * 	   /\ \boxPF \forall x ( EqS(x) -> \boxPF EqS(x) )  (3)
+	 * 		(for each global/ridig role s \in Qrt) 
+	 */
+	public void getExtendedFormula(){
+		Set<QuantifiedRole> qRoles= super.tbox.getQuantifiedRoles1();
+		
+		/* 
+		 * (2)
+		 */
+		Map<Role,List<QuantifiedRole>> qRMap = new HashMap<Role, List<QuantifiedRole>>();
+
+		for(QuantifiedRole qR : qRoles){
+
+			List<QuantifiedRole> list = qRMap.get(qR.getRole());
+
+			if(list == null){
+				list = new ArrayList<QuantifiedRole>();
+				qRMap.put(qR.getRole(),list);
+			}
+			list.add(qR);
+		}
+		
+		for(Entry<Role, List<QuantifiedRole>> e: qRMap.entrySet()){
+			List<QuantifiedRole> qrL = e.getValue();
+
+			Collections.sort(qrL, new Comparator<QuantifiedRole>() {
+				@Override
+				public int compare(QuantifiedRole o1, QuantifiedRole o2) {
+					return o2.getQ()-o1.getQ();
+				}
+			});
+			
+			for(int i = 0;i < qrL.size() - 1;i++){
+				cardinalities.add(new ImplicationFormula(
+						conceptToFormula(qrL.get(i)),
+						conceptToFormula(qrL.get(i + 1))));
+			}
+		}
+		
+		/* 
+		 * (3)
+		 * @TODO: Avoid duplications
+		 */
+
+		Set<Role> roles = super.tbox.getRoles();
+
+		for(QuantifiedRole qR : qRoles){
+			if(qR.getRole().getRefersTo() instanceof AtomicRigidRole){
+				roles.remove(qR.getRole());
+
+				rigidR.add(new ImplicationFormula(
+							conceptToFormula(qR), 
+							conceptToFormula(qR)
+							)
+						);   
+			}
+		}
+		
+		for (Role role : roles) {
+			if(role.getRefersTo() instanceof AtomicRigidRole){
+				QuantifiedRole qRoleRem = new QuantifiedRole(role, 1);
+
+				rigidR.add(new ImplicationFormula(
+								conceptToFormula(qRoleRem), 
+								conceptToFormula(qRoleRem)
+								)
+							);
+			}				
+		}
+		
+	}
+	
+	
+}
--- logics/gario/src/main/java/it/unibz/inf/dllite/DLLiteReasoner.java
+++ logics/gario/src/main/java/it/unibz/inf/dllite/DLLiteReasoner.java
@@ -2,7 +2,8 @@ package it.unibz.inf.dllite;
 
 import it.unibz.inf.qtl1.formulae.Formula;
 import it.unibz.inf.qtl1.output.LatexDocumentCNF;
-
+import it.unibz.inf.qtl1.output.NuSMVOutput;
+import it.unibz.inf.qtl1.output.fo.FOOutput;
 import it.unibz.inf.qtl1.output.pltl.PltlOutput;
 import it.unibz.inf.qtl1.terms.Constant;
 import it.unibz.inf.tdllitefpx.output.LatexOutputDocument;
@@ -27,74 +28,77 @@ public class DLLiteReasoner {
 	 * TBox, ABox SAT
 	 * LTL: TBox|ABox -> QTL -> LTL (NuSMV|NuXMV|Aalta|pltl|TRP++)
 	 * Check TBox and ABox SAT using only LTL pure future formulae
-	 * @param t
-	 * @param verbose
-	 * @param prefix
-	 * @param ABox
-	 * @param purefuture
-	 * @param solver
+	 * @param t an TBox
+	 * @param verbose true/false 
+	 * @param prefix a String
+	 * @param solver a String (Black|NuSMV|all)
 	 * @throws Exception
 	 */
-	public static void buildCheckTBoxSat(
-			TBox t,
-			boolean verbose,
-			String prefix,
-			String solver) 
-					throws Exception{
-		
-		
-			DLLiteReasoner.buildSat(t, verbose, prefix, solver);
-	}
-	
-	/**
-	 * For tdlliteN
-	 */
-	private static void buildSat(
+	public static void checkTBoxSat(
 			TBox t, 
 			boolean verbose, 
-			String prefix, 
+			String prefix,
 			String solver) 
 					throws Exception{
 
 		long total_time = System.currentTimeMillis();
-		
-		System.out.println("TBox|ABox -> Qtl1 -> LTL");
-		
 		long start_tbox2QTL = System.currentTimeMillis();
 		
-		TDLLiteNFPXConverter conv = new TDLLiteNFPXConverter(t);
+		DLLiteConverter conv = new DLLiteConverter(t);
 		Formula qtl_N = conv.getFormula();
 		
 		long end_tbox2QTL = System.currentTimeMillis() - start_tbox2QTL;
-
-		if(verbose) {
-			(new LatexOutputDocument(t)).toFile(prefix+"tbox.tex");
-		}
-		
-		if(verbose)
-			(new LatexDocumentCNF(qtl_N)).toFile(prefix+"qtlN.tex");
-		
-		
 		long start_QTLN2LTL = System.currentTimeMillis();
 
 		Set<Constant> consts = qtl_N.getConstants();
+		if (consts.isEmpty()){
+			consts.add(new Constant("witness"));
+		}
 		
 		Formula ltl = qtl_N.makePropositional(consts);
 		
 		long end_QTLN2LTL = System.currentTimeMillis() - start_QTLN2LTL;
 
+		switch (solver) {
+			case Constants.NuSMV:
+				System.out.println("Solver..." + Constants.NuSMV);
+				(new NuSMVOutput(ltl)).toFile(prefix+".smv");
+			break;
+
+			case Constants.black:
+				System.out.println("Solver" + Constants.black);
+				(new PltlOutput(ltl)).toFile(prefix+".pltl");
+			break;
+
+			case Constants.FO:
+				System.out.println("Generating FO file...");
+				(new FOOutput(qtl_N)).toFile(prefix+".tptp");
+			break;
+			
+			case Constants.all:
+				System.out.println("Solver..." + Constants.NuSMV);
+				(new NuSMVOutput(ltl)).toFile(prefix+".smv");
+				System.out.println("Solver" + Constants.black);
+				(new PltlOutput(ltl)).toFile(prefix+".pltl");
+				System.out.println("Generating FO file...");
+				(new FOOutput(qtl_N)).toFile(prefix+".tptp");
+			break;
+		
+			default:
+				break;
+		}
+		
 		System.out.println("Num of Propositions: " + ltl.getPropositions().size());
 		System.out.println("DLLite to QTL: " + end_tbox2QTL);
 		System.out.println("QTL to LTL: " + end_QTLN2LTL);
-			
-		if(verbose)
-			(new LatexDocumentCNF(ltl)).toFile(prefix+"ltl.tex");
-
-			
-		System.out.println("Solver" + Constants.pltl);
-		(new PltlOutput(ltl)).toFile(prefix+".pltl");
 
 		System.out.println("Done! Total time:" + (System.currentTimeMillis()-total_time) + "ms");
+		
+		if( verbose) {
+			(new LatexOutputDocument(t)).toFile(prefix+"tbox.tex");
+			(new LatexDocumentCNF(qtl_N)).toFile(prefix+"qtlN.tex");
+			(new LatexDocumentCNF(ltl)).toFile(prefix+"ltl.tex");
+		}
 	}
 	
 }
