c26fb05782b162e0bfe0023cea7e8aae308f7775
gbraun <gbraun@gilia.gilia>
2022-04-05 10:29:50 -0300

    DL-lite reasoning: adding abox for checking sat

--- logics/gario/src/main/java/it/unibz/inf/dllite/DLLiteReasoner.java
+++ logics/gario/src/main/java/it/unibz/inf/dllite/DLLiteReasoner.java
@@ -1,5 +1,6 @@
 package it.unibz.inf.dllite;
 
+import it.unibz.inf.qtl1.formulae.ConjunctiveFormula;
 import it.unibz.inf.qtl1.formulae.Formula;
 import it.unibz.inf.qtl1.output.LatexDocumentCNF;
 import it.unibz.inf.qtl1.output.NuSMVOutput;
@@ -9,6 +10,7 @@ import it.unibz.inf.tdllitefpx.output.LatexOutputDocument;
 import it.unibz.inf.tdllitefpx.tbox.TBox;
 import it.unibz.inf.tdllitefpx.Constants;
 import it.unibz.inf.tdllitefpx.TDLLiteNFPXConverter;
+import it.unibz.inf.tdllitefpx.abox.ABox;
 
 import java.util.Set;
 
@@ -90,6 +92,88 @@ public class DLLiteReasoner {
 			(new LatexOutputDocument(t)).toFile(prefix+"tbox.tex");
 			(new LatexDocumentCNF(qtl_N)).toFile(prefix+"qtlN.tex");
 			(new LatexDocumentCNF(ltl)).toFile(prefix+"ltl.tex");
+		}
+	}
+
+	// Considering TBox and ABox
+	/**
+	 * TBox, ABox SAT
+	 * LTL: TBox|ABox -> QTL -> LTL (NuSMV|NuXMV|Aalta|pltl|TRP++)
+	 * Check TBox and ABox SAT using only LTL pure future formulae
+	 * @param t an TBox
+	 * @param a an ABox
+	 * @param verbose true/false 
+	 * @param prefix a String
+	 * @param solver a String (Black|NuSMV|all)
+	 * @throws Exception
+	 */
+	public static void checkKB(
+			TBox t,
+			ABox a, 
+			boolean verbose, 
+			String prefix,
+			String solver) 
+					throws Exception{
+
+		long total_time = System.currentTimeMillis();
+		long start_tbox2QTL = System.currentTimeMillis();
+		
+		// Convert TBox to QTL
+		DLLiteConverter conv = new DLLiteConverter(t);
+		Formula qtl_N = conv.getFormula();
+		
+		long end_tbox2QTL = System.currentTimeMillis() - start_tbox2QTL;
+		long start_QTLN2LTL = System.currentTimeMillis();
+
+		// Get constants
+		Set<Constant> constsABox = a.getConstantsABox();
+		Set<Constant> consts = qtl_N.getConstants();
+		consts.addAll(constsABox);
+
+		// Parse ABox
+		a.addExtensionConstraintsABox(t);
+		Formula o = a.getABoxFormula(true);
+		
+		// Ground the final formula
+		Formula ltl = qtl_N.makePropositional(consts);
+		o = o.makePropositional(consts);
+
+		Formula ltl_KB = new ConjunctiveFormula(ltl, o);
+		
+		long end_QTLN2LTL = System.currentTimeMillis() - start_QTLN2LTL;
+
+		switch (solver) {
+			case Constants.NuSMV:
+				System.out.println("Solver..." + Constants.NuSMV);
+				(new NuSMVOutput(ltl_KB)).toFile(prefix+".smv");
+			break;
+
+			case Constants.black:
+				System.out.println("Solver" + Constants.black);
+				(new PltlOutput(ltl_KB)).toFile(prefix+".pltl");
+			break;
+			
+			case Constants.all:
+				System.out.println("Solver..." + Constants.NuSMV);
+				(new NuSMVOutput(ltl_KB)).toFile(prefix+".smv");
+				System.out.println("Solver" + Constants.black);
+				(new PltlOutput(ltl_KB)).toFile(prefix+".pltl");
+			break;
+		
+			default:
+				break;
+		}
+		
+		System.out.println("Num of Propositions: " + ltl_KB.getPropositions().size());
+		System.out.println("DLLite to QTL: " + end_tbox2QTL);
+		System.out.println("QTL to LTL: " + end_QTLN2LTL);
+
+		System.out.println("Done! Total time:" + (System.currentTimeMillis()-total_time) + "ms");
+		
+		if( verbose) {
+			(new LatexOutputDocument(t)).toFile(prefix+"tbox.tex");
+			(new LatexDocumentCNF(qtl_N)).toFile(prefix+"qtlN.tex");
+			(new LatexDocumentCNF(ltl_KB)).toFile(prefix+"ltl.tex");
 		}
 	}
 	
